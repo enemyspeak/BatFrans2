054ab109b1719a314cdb811125a5e429701e11ad
Kevin Faherty <k@enemyspeak.com>
2017-01-20 23:34:52 -0600

    movement

--- BatFrans/Assets/scripts/movementHandler.cs
+++ BatFrans/Assets/scripts/movementHandler.cs
@@ -3,36 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movementHandler : MonoBehaviour {
-//	public float speed = 4;
-//	public float idleTime = 2.2f;
-//	public float rightHorizontal;
-
 	private bool flipped = false;
 
-	private float jumpVel = 0;
-	private float xspeed = 0;
-	private float yspeed = 0;
+	[SerializeField] private float normal_acceleration = 4;
+	[SerializeField] private float drag_active = 0.9f;
+	[SerializeField] private float drag_passive = 1f;
+	[SerializeField] private int max_speed = 30;
+	[SerializeField] private float deadZone = 0.002f;
+	[SerializeField] private Vector2 velocity = new Vector2(0,0);
 
-	private int direction = 2;
-
-//	private float idleCounter = 0;
-
-	private float normal_acceleration = 4;
-	
-	private float drag_active = 0.9f;
-	private float drag_passive = 0.7f;
-	
-	private int max_speed = 16;
-	private int max_speed_sq = 16 ^ 2;
-
-	public float deadZone = 0.002f;
-//	private float jumpSpeed = 8.5f;
-	private Vector2 velocity = new Vector2(0,0);
-
-//	private int touchingPlatforms = 0;
+	private int max_speed_sq;
 
 	private Animator anim;
-
 	private SpriteRenderer srenderer;
 
 	void Start () {
@@ -40,7 +22,7 @@ public class movementHandler : MonoBehaviour {
 		anim = GetComponent<Animator>();
 		anim.Play("idleStand");
 	}
-//
+
 //	void OnTriggerEnter2D (Collider2D c) {
 //		if(c.CompareTag("OneWayPlatform")) {
 //			touchingPlatforms++;
@@ -89,22 +71,26 @@ public class movementHandler : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		max_speed_sq = max_speed ^ 2;
+
 		bool x_has_input = false;
 		bool y_has_input = false;
 
-		if (Input.GetAxis("Horizontal") != 0) {
+		if (Input.GetAxis("Horizontal") != 0) 
+		{
 			x_has_input = true;
 		}
 
-		if (Input.GetAxis("Vertical") != 0) {
+		if (Input.GetAxis("Vertical") != 0) 
+		{
 			y_has_input = true;
 		}
 
 		float temp_x_accel = Input.GetAxis("Horizontal");
 		float temp_y_accel = Input.GetAxis("Vertical");
 
-		Vector2 temp_norm_accel = normalize_2d(temp_x_accel,temp_y_accel);
+		Vector2 temp_norm_accel = new Vector2(temp_x_accel,temp_y_accel);
 
 		temp_x_accel = temp_norm_accel.x * normal_acceleration;
 		temp_y_accel = temp_norm_accel.y * normal_acceleration;
@@ -112,7 +98,7 @@ public class movementHandler : MonoBehaviour {
 		float temp_x_vel = velocity.x;
 		float temp_y_vel = velocity.y;
 
-		float cur_speed = magnitude_2d(velocity.x, velocity.y);
+		float cur_speed = magnitude_2d (velocity.x, velocity.y);
 
 		if (normal_acceleration + cur_speed > max_speed) {
 
@@ -128,69 +114,24 @@ public class movementHandler : MonoBehaviour {
 		temp_x_vel += temp_x_accel;
 		temp_y_vel += temp_y_accel;
 
-		float temp_vel = magnitude_2d_sq(temp_x_vel, temp_y_vel);
-		if (Mathf.Abs(temp_vel) > max_speed_sq) {
+		float temp_vel = magnitude_2d_sq (temp_x_vel, temp_y_vel);
+		if (Mathf.Abs (temp_vel) > max_speed_sq) {
 			temp_x_vel = velocity.x;
 			temp_y_vel = velocity.y;
 		}
 
 		float temp_drag = drag_passive;
-		if (x_has_input || y_has_input) {
+		if (y_has_input || x_has_input) {
 			temp_drag = drag_active;
 		}
-		
-		velocity = new Vector2(temp_x_vel * temp_drag * Time.deltaTime,temp_y_vel * temp_drag * Time.deltaTime);
+			
+		velocity = new Vector2 (temp_x_vel * temp_drag, temp_y_vel * temp_drag);
 
-//
-////		if (Input.GetButton("Fire1")) {
-////			barking = true;
-////		} else if (Input.GetButton("Jump") && !jumping && (transform.position.y > 0) && Input.GetAxis("Vertical") < 0) {
-////			jumping = true;
-////			jumpVel = 0;
-////			//	Vector2 pos = transform.position;
-////			//	pos.y -= 0.1f;
-////			//	transform.position = pos;
-////			xspeed = Input.GetAxis("Horizontal");
-////		} else if (Input.GetButton("Jump") && !jumping) {
-////			jumping = true;
-////			jumpVel = 4.8f;
-////			xspeed = Input.GetAxis("Horizontal");
-////		} else {
-////			barking = false;
-////		}
-//
-////		if (barking) {
-//			velocity = new Vector2 (0,0);
-////		} else if (jumping) {
-////			jumpVel -= jumpSpeed * Time.deltaTime;
-////			if ( transform.position.y < 0) {
-////				Vector2 currentPosition2 = transform.position;
-////				jumpVel = 0;
-////				jumping = false;
-////				transform.position = new Vector2(currentPosition2.x, 0);
-////			}
-////			velocity = new Vector2(xspeed * Time.deltaTime,jumpVel * Time.deltaTime);
-////		} else {
-////			if (Input.GetAxis("Horizontal") != 0) {
-////				walking = true;
-////			} else {
-////				walking = false;
-////			}
-//			xspeed = Input.GetAxis("Horizontal");
-//			if (xspeed < 0 && !flipped) {
-//				flip();	
-////				walking = false;
-//			} else if (xspeed > 0 && flipped) {
-//				flip();	
-////				walking = false;
-//			}
-//			velocity = new Vector2(xspeed * Time.deltaTime,0);
-////		}
 		Vector2 currentPosition = transform.position;
-		transform.position = new Vector2(currentPosition.x + velocity.x * speed, currentPosition.y + velocity.y);
-//
-//		transform.position = clipPlayer(-5.6f,-0.8f,rightHorizontal,4 );
-//
+		transform.position = new Vector2(currentPosition.x + velocity.x, currentPosition.y + velocity.y);
+
+//		previousVelocity = velocity;
+
 		doAnimation();
 	}
 
