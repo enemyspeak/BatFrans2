1a9653af914d40117c9e56796f13a6c01e9eb242
Kevin Faherty <k@enemyspeak.com>
2017-01-22 14:38:45 -0600

    cinematic code

--- BatFrans/Assets/scripts/loverBatHandler.cs
+++ BatFrans/Assets/scripts/loverBatHandler.cs
@@ -20,22 +20,24 @@ public class loverBatHandler : MonoBehaviour {
 	void Update () {
 		if (cinemationRunning) {
 			Vector2 currentPosition = transform.position;
-
+			float speed = 2.0f;
 			if (flyToTarget) {
 //				target = new Vector2(
 //					Mathf.Lerp (currentPosition.x, cinemationPosition.transform.position.x, 0.3f * Time.deltaTime),
 //					Mathf.Lerp (currentPosition.y, cinemationPosition.transform.position.y, 0.3f * Time.deltaTime));
 				target = cinemationPosition.transform.position;
 			} else {
-				float x = Mathf.Sin( 2.0f * time * 1.0f ) * ( Mathf.Sin (0.01f * time * 1.0f) * 250);
-				float y = Mathf.Cos( 2.0f * time * 1.0f ) * ( Mathf.Sin (0.01f * time * 1.0f) * 250);
+				Debug.Log (time);
+				float x = Mathf.Sin( 1.0f * time * speed ) * ( Mathf.Sin (0.01f * time * speed ) * 200 );
+				float y = Mathf.Cos( 1.0f * time * speed ) * ( Mathf.Sin (0.01f * time * speed ) * 200 );
 				target = new Vector2 (currentPosition.x + x,currentPosition.y + y);// + height / 2);
 			}
 				
 			transform.position = new Vector3 (
-				Mathf.Lerp (currentPosition.x, target.x, 0.15f * time),
-				Mathf.Lerp (currentPosition.y, target.y, 0.15f * time),
+				Mathf.Lerp (currentPosition.x, target.x, 0.15f * Time.deltaTime),
+				Mathf.Lerp (currentPosition.y, target.y, 0.15f * Time.deltaTime),
 				-10);
+			
 			time += Time.deltaTime;
 		}
 	}
@@ -44,7 +46,7 @@ public class loverBatHandler : MonoBehaviour {
 		cinemationRunning = true;
 		Debug.Log ("running cinemation");
 		StartCoroutine (startEndingMovement ());
-		time = 0;
+//		time = 0;
 	}
 
 	IEnumerator startEndingMovement() {
